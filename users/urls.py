e186ee6368d94db3b8f0f7448929cf3ae47203bf
gangyuji <gangyoyul9@gmail.com>
2023-04-30 14:24:04 +0900

    blacklist test

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('signup/', views.UserView.as_view(), name='user_view'),
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('<int:user_id>/', views.ToDoListUserView.as_view(), name='todolist_user'),
 ]
